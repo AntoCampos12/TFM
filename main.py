21a3024fa163f4f0f29a88babafc833c9b69ac1b
AntoCampos12 <drapionx1@gmail.com>
2024-08-31 14:21:12 +0200

    CÓDIGO FINALIZADO
    Los archivos han sido aclarados con comentarios y explicaciones, además se ha eliminado código innecesario

--- main.py
+++ main.py
@@ -21,108 +21,71 @@ CODIGOS = ['MCF0600', 'MAS0025', 'NAH0503', 'LBC0356', 'SSH0799', 'LJT0817', 'AN
            'SKG0759', 'AAM0658', 'MAR0955', 'KLH0596', 'DGM0754', 'MBW0809', 'HJS0072', 'LPH0572', 'MTT0901', 'EDA0684', 
            'CRD0624', 'LDD0560', 'EAH0466', 'JRH0455', 'BQS0525', 'DAR0885', 'CAC0889', 'CFW0264', 'AAS0442', 'LHB0606', 
            'OLB0749', 'HMM0108', 'CYA0506', 'SLT0907']
-PLOT4 = [0.44460591382105025, 0.6370889678382262, 0.9117704213259056, 0.8917290636126908, 0.8903274267533648, 0.8944271909999159, 0.8987170342729172, 0.9268086959962983, 
-         0.9058216273156766, 0.9128709291752769, 0.9336995618478525, 0.9318911162960933, 0.9328075204080797, 0.9258200997725514, 0.9459053029269173, 0.9528351047490354, 
-         0.9591663046625439, 0.9586025865388216, 0.6765696408155183, 0.6821127309893709, 0.67700320038633, 0.6813851438692469, 0.6083989264712285, 0.6813851438692469, 
-         0.6691496051182058, 0.6797949780052895, 0.672021505032247, 0.9503819266229829, 0.947789578306157, 0.6666666666666666, 0.6859943405700354, 0.6715507368448513, 
-         0.6633249580710799, 0.7989354619369612, 0.674199862463242, 0.9636241116594315, 0.6698641270570836, 0.9597148699373931, 0.6853444168423419, 0.9710083124552245, 
-         0.6839166144230384, 0.7071067811865476, 0.693888666488711, 0.693888666488711, 0.916515138991168, 0.9789450103725609]
-PLOT3 = [0.5806972553108731, 0.8043996665398437, 0.8431152788877544, 0.8431152788877544, 0.8410214463203252, 0.8440971508067067, 0.8243101233681287, 0.9198662110077999, 
-         0.8397191227596316, 0.8397191227596316, 0.8623164985025763, 0.8735890880367281, 0.8603834844182798, 0.8374357893586238, 0.835085876453812, 0.8735890880367281, 
-         0.9521904571390466, 0.86991767240168, 0.6166698388447784, 0.6236095644623235, 0.6123724356957945, 0.6654751256486924, 0.7803558315797974, 0.6267831705280087, 
-         0.5086319569803804, 0.6276459144608478, 0.6413192049055175, 0.8890008890013334, 0.873333764609373, 0.6161409170227454, 0.6642111641550714, 0.6262242910851494, 
-         0.6164414002968976, 0.7145896010104964, 0.6123724356957945, 0.8864052604279183, 0.6201736729460423, 0.8735890880367281, 0.6396021490668313, 0.8783100656536799, 
-         0.6090712125322324, 0.7071067811865476, 0.5773502691896257, 0.90267093384844, 0.848528137423857, 0.9789450103725609]
-PLOT2 = [0.8141195843820602, 0.7896387856258745, 0.8140335388727166, 0.8065992869067975, 0.8410214463203252, 0.82915619758885, 0.8473185457363234, 0.869718492622904, 
-         0.8320502943378437, 0.8320502943378437, 0.869718492622904, 0.8506963092234007, 0.8603834844182798, 0.8678978933300583, 0.8735890880367281, 0.8735890880367281, 
-         0.8944271909999159, 0.86991767240168, 0.6223535519798788, 0.6236095644623235, 0.6346477588219923, 0.6324555320336759, 0.5730254871776856, 0.6324555320336759, 
-         0.6464599351554542, 0.6336522323129238, 0.6413192049055175, 0.8890008890013334, 0.9019752336033946, 0.6382847385042254, 0.6492831643058214, 0.8631906158060839, 
-         0.8831760866327847, 0.8991721961325718, 0.5075192189225523, 0.8728715609439694, 0.609749843620211, 0.8885233166386386, 0.6276459144608478, 0.9102589898327995, 
-         0.5956833971812706, 0.6526300069150406, 0.6085806194501846, 0.90267093384844, 0.938083151964686, 0.9354143466934853]
-PLOT =  [0.44786314354815715, 0.6277878997390947, 0.8984591897591402, 0.8917290636126908, 0.8903274267533648, 0.8944271909999159, 0.9058216273156766, 0.9268086959962983, 
-         0.9128709291752769, 0.9198662110077999, 0.9198662110077999, 0.9318911162960933, 0.9397429877987296, 0.9258200997725514, 0.9389243565742775, 0.9528351047490354, 
-         0.9660917830792959, 0.9515279320153484, 0.6817541583256872, 0.67700320038633, 0.6718548123582124, 0.6761234037828132, 0.6083989264712285, 0.6813851438692469, 
-         0.6691496051182058, 0.674199862463242, 0.6659942845826639, 0.9503819266229829, 0.947789578306157, 0.6666666666666666, 0.6859943405700354, 0.6715507368448513, 
-         0.6633249580710799, 0.7989354619369612, 0.6657190234489458, 0.9636241116594315, 0.6698641270570836, 0.9733285267845752, 0.6963106238227914, 0.9710083124552245, 
-         0.672021505032247, 0.7071067811865476, 0.693888666488711, 0.6804138174397717, 0.938083151964686, 0.9789450103725609]
 
+# Se inicializa el diccionario que se rellenara con los resultados de cada una de las evaluaciones
 diccionario = []
 for i in range(0, 53):
     diccionario.append({"VP": 0, "VN": 0, "FP": 0, "FN": 0})
 
-def __calcular_rendimiento(diccionario_evaluaciones):
-    rendimiento_semana = []
-    for evaluacion in diccionario_evaluaciones:
-        if evaluacion["VP"] + evaluacion["FN"] == 0:
-            tasa_VP = 1
-        else:
-            if evaluacion["VP"] / (evaluacion["VP"] + evaluacion["FN"]) == 0:
-                tasa_VP = 0.5
-            else:
-                tasa_VP = evaluacion["VP"] / (evaluacion["VP"] + evaluacion["FN"])
-        if evaluacion["VN"] + evaluacion["FP"] == 0:
-            tasa_VN = 1
-        else:
-            tasa_VN = evaluacion["VN"] / (evaluacion["FP"] +  evaluacion["VN"])
-        rendimiento_semana.append(np.sqrt(tasa_VP * tasa_VN))
-    return rendimiento_semana
-
-def get_numero_actividades(secuencia):
+# En ocasiones para las pruebas se filtraron las observaciones por su número de actividades
+def __get_numero_actividades(secuencia):
     flat_map = lambda f, xs: reduce(lambda a, b: a + b, map(f, xs))
     actividades_usuario = flat_map(lambda x: x, secuencia.values())
     return len(actividades_usuario)
 
-def main(codigo):
+# La función principal, realiza los siguientes pasos:
+def main(codigo, mostrarEvaluacion):
     print("==================== INICIALIZACIÓN DE LOS DATOS ==========================")
-    secuencia = input.get_secuencia_local(codigo)
+    secuencia = input.get_secuencia_local(codigo) # Del archivo local se obtiene el diccionario de observaciones del usuario 
     print("\nGENERADA SECUENCIA DE OBSERVACIONES DE {}:\n{}".format(codigo, secuencia))
     print("==================== OBTENCIÓN DE DATAFRAME PARA APLICACIÓN BAUM WELCH ==========================")
-    secuencia_semana = input.obtener_semana_usuario(secuencia,1)
-    print("ACTIVIDADES POR SEMANA {}\n".format(get_numero_actividades(secuencia)))
+    secuencia_semana = input.obtener_semana_usuario(secuencia,1) # Se transforma la semana de actividades en un dataFrame de panda
+    print("ACTIVIDADES POR SEMANA {}\n".format(__get_numero_actividades(secuencia)))
     print("==================== OBTENER MATRICES DE EMISIÓN Y TRANSICIÓN ==========================")
-    
-    dataframe, transicion, emision = BaumWelch.iniciar_BaumWelch(secuencia_semana)
-    plot_x = range(6, len(secuencia.keys()))
+    dataframe, transicion, emision = BaumWelch.iniciar_BaumWelch(secuencia_semana) # Se inicializa las matrices de transicion, emision y valores iniciales del HMM
+    plot_x = range(6, len(secuencia.keys())) # Se inicializan también las variables que luego permitirán mostrar en una gráfica los valores obtenidos
     plot_y = []
 
+    # Durante las primeras cinco semanas solo se entrena el modelo
     for i in range(1,6):    
         transicion, emision = BaumWelch.aplicar_entrenamiento(secuencia_semana, 100, 0.01, 0.1, 5, 0.5, transicion, emision)
         secuencia_semana = input.obtener_semana_usuario(secuencia, i)
 
+    # A partir de la semana cinco, se evalúa la secuencia obtenida y se entrena el módelo con la mejor secuencia estimada
     for i in range(6, len(secuencia.keys())):
         prob = Predict.viterbi(transicion, emision, BaumWelch.NUMERO_ESTADOS, secuencia[i])
         plot_y.append(prob[0])
         secuencia_semana = input.parse_to_dataframe(prob[1])
         transicion, emision = BaumWelch.aplicar_entrenamiento(secuencia_semana, 100, 0.01, 0.1, 5, 0.5, transicion, emision)
-        #secuencia_semana = input.obtener_semana_usuario(secuencia, i)
 
+    # Se evalúan los resultados obtenidos y se muestra en una gráfica las probabilidades recogidas en cada semana
     resultados_semana = Evaluacion.getEvaluacionSemanas(codigo, plot_y)
     Evaluacion.actualizarDiccionarioEvaluaciones(resultados_semana,diccionario)
     print("============================ EVALUACIÓN COMPLETADA ===========================")
-    #plt.plot(plot_x, plot_y)
-    #plt.show()
-    
-    # print("TRANSICIÓN:", transicion)
-    # print("EMISION", emision)
-    # print("=========================== PREDICCIÓN ====================================")
-    # prob, ruta = Predict.viterbi(transicion, emision, BaumWelch.NUMERO_ESTADOS, dataframe_semana1['actividad'].values, BaumWelch.INICIAL[0])
-    # print("PROBABILIDAD",prob)
-    # print("PREDECCIÓN RUTA",ruta)
-    # UTILIZAR LAS PRIMERAS CINCO SEMANAS PARA ENTRENAR EL MODELO
-    # PROBABILIDAD <- PREDECIR LA SECUENCIA A PARTIR DEL MODELO
+    if(mostrarEvaluacion):
+        plt.plot(plot_x, plot_y)
+        plt.show()
 
-start = time.time()
-for i in CODIGOS:
-    secuencia = input.get_secuencia_local(i)
-    tam = get_numero_actividades(secuencia)
-    main(i)
-end = time.time()
 
-print("TIEMPO", end - start)
-d = [(i,diccionario[i]) for i in range(0,len(diccionario))]
-print("DICCIONARIO EVALUACIONES", d)
-rendimiento = __calcular_rendimiento(diccionario[:46])
-print("RENDIMIENTO", rendimiento)
-plot_x = range(0, len(rendimiento))
-plt.plot(plot_x, rendimiento)
-plt.show()
+# Si se quiere ejecutar el algoritmo aquí se presentan dos opciones:
+#   • La iteración individual realiza una ejecución completa para un único individuo, muestra las probabilidades del individuo para cada semana
+#   • La iteración conjunta realiza la ejecución de todos los códigos que se encuentran en el archivo local, muestra el rendimiento obtenido
+def iteracion_individual():
+    main('MCF0600', True)
+
+def iteracion_conjunta():
+    start = time.time()
+    for i in CODIGOS:
+        main(i, False)
+    end = time.time()
+
+    print("TIEMPO", end - start)
+    d = [(i,diccionario[i]) for i in range(0,len(diccionario))]
+    print("DICCIONARIO EVALUACIONES", d)
+    rendimiento = Evaluacion.calcular_rendimiento(diccionario[:46])
+    print("RENDIMIENTO", rendimiento)
+    plot_x = range(0, len(rendimiento))
+    plt.plot(plot_x, rendimiento)
+    plt.show()
+
+iteracion_individual()
